4156144181a768ce0c90d59699ed44f24a49f7ea
姚正亮 <yaozhengliang@chaomengdata.com>
2018-06-15 19:17:54 +0800

    响应式完成

---
+++ src/components/blogList.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="blogList">
+    <div class="blogImg" v-if="blogimage" :style="{backgroundImage: 'url('+blogimage+')', backgroundSize: 'cover'}"></div>
+    <section>
+      <h2>{{blogtitle}}</h2>
+      <p class="description">{{blogdescription}}</p>
+    </section>
+    <footer>
+      <ul class="tags">
+        <li v-for="item in blogtags" :key="item" class="tag">{{item}}</li>
+      </ul>
+      <time>{{date}}</time>
+    </footer>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  data () {
+    return {
+      
+    }
+  },
+
+  props: {
+    blogimage: {
+      type: String,
+      default: null,
+      required: false
+    },
+    blogtitle: {
+      type: String,
+      default: null,
+      required: true
+    },
+    blogdescription: {
+      type: String,
+      default: null,
+      required: true
+    },
+    blogtags: {
+      type: Array,
+      default: () => [],
+      required: false
+    },
+    blogdate: {
+      type: String,
+      default: null,
+      required: true
+    },
+    blogid: {
+      type: String,
+      default: null,
+      required: true
+    }
+  },
+
+  computed: {
+    date: () =>{
+      return moment(this.blogdate).format('YYYY-MM-DD HH:mm:ss');
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@media only screen and (max-width: 700px) {
+  .blogImg{
+    height: 250px !important;
+  }
+  section{
+    h2{
+      font-size: 18px !important;
+    }
+  }
+  footer{
+    .tags{
+      display: none !important;
+    }
+    time{
+      float: left !important;
+      text-align: left !important;
+    }
+  }
+}
+.blogList{
+  padding: 30px 20px;
+  background: #fff;
+  .blogImg{
+    width: 100%;
+    height: 300px;
+    background-position: center;
+    background-repeat: no-repeat;
+    margin-bottom: 14px;
+  }
+  >section{
+    h2{
+      color: #4e4e4e;
+      font-size: 24px;
+      padding-left: 10px;
+    }
+    .description{
+      font-size: 16px;
+      line-height: 26px;
+      color: #656565;
+    }
+  }
+  >footer{
+    height: 22px;
+    margin-top: 16px;
+    .tags{
+      max-width: 70%;
+      overflow: hidden;
+      float: left;
+      .tag{
+        display: inline-block;
+        height: 22px;
+        line-height: 22px;
+        padding: 0 8px;
+        color: #a6abb2;
+        background: #fafafa;
+        font-size: 14px;
+      }
+    }
+    time{
+      float: right;
+      text-align: right;
+      color: #a9b0bc;
+      font-size: 14px;
+      line-height: 22px;
+    }
+  }
+}
+</style>
+
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,102 +1,189 @@
 <template>
-  <div class="home">
-    <header>
-      <nav class="nav"></nav>
-    </header>
+	<div class="home">
+		<header>
+			<nav class="nav">
+				<div class="logo"></div>
+				<div class="routeBtn">
+					<a class="home-route">主页</a>
+				</div>
+			</nav>
+			<div class="welcome">
+				<h1>Welcome to my blog</h1>
+				<p>欢迎来到我的博客</p>
+			</div>
+		</header>
 
-    <div class="main">
-      <div class="row">
-        <article></article>
-        <aside></aside>
-      </div>
-    </div>
-  </div>
+		<div class="main">
+			<div class="row">
+				<article>
+          <template v-for="item in val">
+            <BlogList :key="item.id" :blogimage="item.image" :blogtitle="item.title" :blogdescription="item.description" :blogid="item.id" :blogdate="item.date" :blogtags="item.tags"></BlogList>
+          </template>
+				</article>
+				<aside></aside>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import BlogList from '../components/blogList'
 export default {
-  created () {
-    
+  data() {
+    return {
+      val: [
+        { "image": "http://pa919xbba.bkt.clouddn.com/5b2223c8c27f9c2c9a499e66.jpg", "title": "第一篇博客文章", "tags": ["学习"], "description": "看到别的大牛自己的博客一同有些羡慕，于是用了一周多时间完成了一个博客系统的构建。虽然简陋点，但是还能记录一些知识。方便复习与总结。", "date": "2018-06-14T08:39:09.514Z", "id": "5b222969c27f9c2c9a499e68" }
+        ]
+    }
+  },
+  components: {
+    BlogList: BlogList
   }
+
 };
+
 </script>
 
 <style lang="scss" scoped>
-@media only screen and (max-width: 700px) {
-  .row {
-    width: 100%;
-  }
-  article {
-    width: 100%;
-  }
-  aside {
-    display: none;
-  }
-  header {
-    height: 400px;
-  }
-}
-@media only screen and (max-width: 960px) and (min-width: 700px){
-  .row{
-    width: 700px;
-    margin: 0 auto;
-  }
-  article {
-    width: 700px;
-  }
-  aside {
-    display: none;
-  }
-  header {
-    height: 420px;
-  }
-}
-@media only screen and (min-width: 960px) {
-  .row{
-    width: 960px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-around;
-  }
-  article {
-    width: 700px;
-  }
-  aside {
-    width: 200px;
-  }
-  header {
-    height: 420px;
-  }
-}
-.home {
-  background-image: url(http://oxnqkx7p0.bkt.clouddn.com/cassie-boca-293379-unsplash.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed;
-  height: 1090px;
-  header {
-    position: relative;
-    .nav {
-      position: absolute;
-      top: 0;
-      width: 100%;
-      height: 60px;
-      background: rgba(255, 255, 255, 0.5);
-    }
-  }
-  .main {
-    background: #fff;
-    width: 100%;
-  }
-  article {
-    background: #ccf;
-    height: 800px;
-  }
-  aside {
-    background: #aaa;
-    height: 800px;
-  }
-}
-</style>
+	@media only screen and (max-width: 700px) {
+		.row {
+			width: 100%;
+		}
+		article {
+			width: 100%;
+		}
+		aside {
+			display: none;
+		}
+		header {
+			height: 400px;
+			.routeBtn {
+				display: none !important;
+			}
+			>.welcome {
+				padding-top: 100px;
+				>h1 {
+					line-height: 95px;
+					font-size: 2.2em !important;
+				}
+				>p {
+					font-size: 1.4em !important;
+				}
+			}
+		}
+	}
 
+	@media only screen and (max-width: 960px) and (min-width: 700px) {
+		.row {
+			width: 700px;
+			margin: 40px auto;
+		}
+		article {
+			width: 700px;
+		}
+		aside {
+			display: none;
+		}
+		header {
+			height: 420px;
+			>.welcome {
+				padding-top: 105px;
+			}
+		}
+	}
+
+	@media only screen and (min-width: 960px) {
+		.row {
+			width: 960px;
+			margin: 40px auto;
+			display: flex;
+			justify-content: space-around;
+		}
+		article {
+			width: 700px;
+		}
+		aside {
+			width: 200px;
+		}
+		header {
+			height: 420px;
+			>.welcome {
+				padding-top: 105px;
+			}
+		}
+	}
+
+	.home {
+		background-image: url(../assets/images/aa33566bad.jpg);
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+		background-attachment: fixed;
+		height: 1090px;
+		header {
+			position: relative;
+			.nav {
+				position: absolute;
+				top: 0;
+				width: 100%;
+				height: 60px;
+				background: rgba(255, 255, 255, 0.5);
+				.logo {
+					margin-left: 2%;
+					height: 60px;
+					width: 200px;
+					float: left;
+					background-image: url(../assets/images/logo.png);
+					background-repeat: no-repeat;
+					background-size: auto 100%;
+				}
+				.routeBtn {
+					height: 60px;
+					width: 200px;
+					display: flex;
+					margin-right: 2%;
+					align-items: center;
+					justify-content: center;
+					float: right;
+					a {
+						font-size: 18px;
+						display: inline-block;
+						padding: 0 10px;
+						cursor: pointer;
+					}
+				}
+			}
+			.welcome {
+				height: 150px;
+				text-align: center;
+				>h1 {
+					color: #fff;
+					height: 110px;
+					line-height: 60px;
+					margin: 0;
+					font-size: 3rem;
+					padding: 25px 0;
+				}
+				>p {
+					color: #fff;
+					height: 40px;
+					line-height: 40px;
+					font-size: 2rem;
+				}
+			}
+		}
+		.main {
+			background: #f7f8fa;
+      width: 100%;
+      overflow: hidden;
+		}
+		article {
+			height: 800px;
+		}
+		aside {
+			background: #aaa;
+			height: 800px;
+		}
+	}
+
+</style>
